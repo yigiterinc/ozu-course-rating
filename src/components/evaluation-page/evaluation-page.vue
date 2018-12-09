10b7ca748a4ed9c467135dc269066bc3c853ce0c
Yigit Erinc <yigit.erinc@ozu.edu.tr>
2018-12-09 07:48:42 +0300

    revise the answering system, refactor the code

--- src/components/evaluation-page/evaluation-page.vue
+++ src/components/evaluation-page/evaluation-page.vue
@@ -5,27 +5,9 @@
       <hr>
       <br>
       <div class="container">
-      <b-form @submit="this.onSubmit">
-        <question v-for="(question,index) in questions" :questionText="question.questionText" :question-index="index"></question>
-        <br>
-        <p class="comments-label"><strong>Additional Comments and Suggestions</strong></p>
-        <hr>
-        <p>If you have any additional comments please write them below.</p>
-        <b-form-textarea v-model="additionalComments"
-                         placeholder="Write your answer to here"
-                         :rows="3"
-                         :max-rows="6">
-        </b-form-textarea>
-        <br>
-        <p>What would you suggest to other students who aim to pass this course with a high grade?</p>
-        <b-form-textarea v-model="suggestions"
-                         placeholder="Write your answer to here"
-                         :rows="3"
-                         :max-rows="6">
-        </b-form-textarea>
-        <br>
-        <b-button type="submit" variant="success">Submit</b-button>
-      </b-form>
+        <evaluation-form :additional-comments="this.additionalComments"
+                         :questions="this.questions" :suggestions="this.suggestions" :course="this.course">
+        </evaluation-form>
       </div>
       <br>
     </div>
@@ -35,14 +17,14 @@
 
 <script>
   import Course from './course.json'
-  import Question from './question.vue'
-  import QuestionList from './question-list.json' // question list is going to be fetched from db by backend
+  import QuestionList from './question-list.json'
+  import EvaluationForm from "./evaluation-form"; // question list is going to be fetched from db by backend
 
   export default {
       name: 'evaluation-page',
       components: {
-        Question,
-      },
+        EvaluationForm,
+        },
       // props: ['course'] // a course object is passed when it is called from search component
       data() {
         return {
@@ -51,19 +33,8 @@
           additionalComments: "",
           suggestions: ""
         }
-      },
-    methods: {
-      onSubmit (evt) {
-        // submit user's answers to database. foreach question, call submitAnswer
-        // redirect to results page for that course
-      },
-    }
+      }
     }
 </script>
 
 
-<style scoped>
- .comments-label {
-   font-size: 150%;
- }
-</style>
