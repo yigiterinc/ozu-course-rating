3ba14cc8e232bf69d21930c2b2a57894efeaeb2b
Sarp Ozturk <sarp.ozturk@ozu.edu.tr>
2018-12-23 22:52:24 +0300

    implement comments and suggestion page

---
+++ src/components/comments-and-suggestion-page/comments-and-suggestion-page.vue
@@ -0,0 +1,44 @@
+<template>
+  <b-container>
+    <nav-bar></nav-bar>
+    <h1 id="commentHeader">Comments About The Course</h1>
+    <hr>
+      <comment-section></comment-section>
+    <h1 id="suggestionHeader">Suggestions For The Course</h1>
+    <hr>
+    <suggestion-section></suggestion-section>
+    <div id="endOfPage"></div>
+  </b-container>
+</template>
+
+<script>
+  import NavBar from "../nav-bar/nav-bar"
+  import CommentSection from "../comments-and-suggestion-page/comments-section"
+  import SuggestionSection from "../comments-and-suggestion-page/suggestions-section"
+
+    export default {
+        name: "comments-and-suggestion-page",
+        components: {
+          NavBar,
+          CommentSection,
+          SuggestionSection
+        }
+    }
+</script>
+
+<style scoped>
+  hr {
+    height: 2px;
+    background-color: black;
+    opacity: 0.5;
+  }
+
+  #suggestionHeader {
+    margin-top: 90px;
+  }
+
+  #endOfPage {
+    margin-bottom: 90px;
+  }
+
+</style>
---
+++ src/components/comments-and-suggestion-page/comments-section.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-container>
+    <b-row >
+        <div v-for="user in users">
+          <b-col>
+          <b-card bg-variant="light" v-bind:title=user.nickname
+                  tag="article"
+                  style="max-width: 20rem;"
+                  class="mb-2">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </p>
+          </b-card>
+          </b-col>
+        </div>
+    </b-row>
+
+  </b-container>
+</template>
+
+<script>
+
+    export default {
+        name: "comments-section",
+      data() {
+          return {
+
+            users: [
+              {nickname: "user1", comment:"Very good. Very nice. Take it Very good. Very nice. Take it Very good. Very nice. Take it Very good. Very nice. Take it"},
+              {nickname: "user2", comment:"not bad"},
+              {nickname: "user3", comment:"dont recommend"},
+              {nickname: "user4", comment:"dont recommend"},
+              {nickname: "user5", comment:"dont recommend"},
+            ]
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
